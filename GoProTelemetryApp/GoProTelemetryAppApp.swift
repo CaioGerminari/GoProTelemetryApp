7a6290419172cc47d81b0b995a63d98f935d6d8f
Caio Germinari <caiogerminari@icloud.com>
2025-11-30 10:43:06 -0300

    Initial Commit

---
+++ GoProTelemetryApp/GoProTelemetryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoProTelemetryAppApp.swift
+//  GoProTelemetryApp
+//
+//  Created by Caio Germinari on 30/11/25.
+//
+
+import SwiftUI
+
+@main
+struct GoProTelemetryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
